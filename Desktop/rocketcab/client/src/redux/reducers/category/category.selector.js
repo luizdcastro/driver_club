28062308965dcf3cff1303c2888b6e798e547f01
Luiz Castro <lfernando.cwb@gmail.com>
2020-07-02 19:04:04 -0300

    folders change

--- Desktop/rocketcab/client/src/redux/reducers/category/category.selector.js
+++
@@ -1,8 +0,0 @@
-import { createSelector } from 'reselect';
-
-const selectCategory = (state) => state.category;
-
-export const selectCategorySections = createSelector(
-  [selectCategory],
-  (category) => category.categories
-);
