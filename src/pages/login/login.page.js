869c3e98910bea736bbe0f2483744b1a06d0fd36
unknown <luizf.dcastro@gmail.com>
2020-07-26 12:12:07 -0300

    api server change

--- src/pages/login/login.page.js
+++ src/pages/login/login.page.js
@@ -7,26 +7,26 @@ import FormInput from '../../components/form-input/form-input.component';
 import CustomButton from '../../components/custom-button/custom-button.component';
 import './login.styles.css';
 
-const Login = ({ dispatchLoginAction }) => {
+const Login = ({ dispatchLoginAciton }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [serverError, setServerError] = useState('');
 
-  function handleOnSubmmit(event) {
+  const handleOnSubmmit = (event) => {
     event.preventDefault();
-    dispatchLoginAction(
+    dispatchLoginAciton(
       email,
       password,
       () => console.log('Logedd In'),
       (message) => setServerError(message)
     );
-  }
+  };
 
   return (
     <div className="login-container">
       <div className="login">
         <h2 className="login-title">Entrar na sua conta</h2>
-        <form>
+        <form onSubmit={handleOnSubmmit}>
           <FormInput
             type="email"
             name="email"
@@ -42,8 +42,6 @@ const Login = ({ dispatchLoginAction }) => {
             handleChange={(e) => setPassword(e.target.value)}
           />
           <CustomButton name="Login" onClick={handleOnSubmmit} />
-          <CustomButton name="Teste 1" onClick={() => alert('teste')} />
-
           <div className="login-link__container">
             <Link className="login-link">Esqueceu sua senha?</Link>
             <Link to="/register" className="login-link">
@@ -58,7 +56,7 @@ const Login = ({ dispatchLoginAction }) => {
 };
 
 const mapDispatchToProps = (dispatch) => ({
-  dispatchLoginAction: (email, password, onSuccess, onError) =>
+  dispatchLoginAciton: (email, password, onSuccess, onError) =>
     dispatch(loginUser({ email, password }, onSuccess, onError)),
 });
 
