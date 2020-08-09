b242b869994cf2b1c0fe4db5b9a9259695e4e6c9
unknown <luizf.dcastro@gmail.com>
2020-08-09 11:37:53 -0300

    home page grid

---
+++ src/components/disconts-calculator/disconts-calculator.component.jsx
@@ -0,0 +1,88 @@
+import React, { useEffect, useState } from 'react';
+
+import CustomButton from '../../components/custom-button/custom-button.component';
+import FormInput from '../../components/form-input/form-input.component';
+import './disconts-calculator.styles.css';
+
+const DiscontCalculator = () => {
+  const [total, setTotal] = useState('');
+  const [combustivel, setCombustivel] = useState('');
+  const [alimentacao, setAlimentacao] = useState('');
+  const [seguro, setSeguro] = useState('');
+  const [manutencao, setManutencao] = useState('');
+  const [locacao, setLocacao] = useState('');
+
+  useEffect(() => {
+    setTotal(
+      Number(combustivel) +
+        Number(alimentacao) +
+        Number(seguro) +
+        Number(manutencao) +
+        Number(locacao)
+    );
+  }, [combustivel, alimentacao, seguro, manutencao, locacao]);
+  return (
+    <div className="home-page__calculator">
+      <form className="home-calculator__container">
+        <h2 className="home-calculator__title">
+          Calcule quanto você pode economizar por mês:
+        </h2>
+        <ul>
+          <li className="home-calculator__items">
+            <p>Combustível</p>
+            <FormInput
+              id="calculator-input"
+              type="number"
+              placeholder="R$ 0,00"
+              handleChange={(e) => setCombustivel((e.target.value / 100) * 7)}
+            />
+          </li>
+          <li className="home-calculator__items">
+            <p>Alimentação</p>
+            <FormInput
+              id="calculator-input"
+              type="number"
+              placeholder="R$ 0,00"
+              handleChange={(e) => setAlimentacao((e.target.value / 100) * 15)}
+            />
+          </li>
+          <li className="home-calculator__items">
+            <p>Seguro</p>
+            <FormInput
+              id="calculator-input"
+              type="number"
+              placeholder="R$ 0,00"
+              handleChange={(e) => setSeguro((e.target.value / 100) * 10)}
+            />
+          </li>
+          <li className="home-calculator__items">
+            <p>Manutenção</p>
+            <FormInput
+              id="calculator-input"
+              type="number"
+              placeholder="R$ 0,00"
+              handleChange={(e) => setManutencao((e.target.value / 100) * 10)}
+            />
+          </li>
+          <li className="home-calculator__items">
+            <p>Aluguel de Carro</p>
+            <FormInput
+              id="calculator-input"
+              type="number"
+              placeholder="R$ 0,00"
+              handleChange={(e) => setLocacao((e.target.value / 100) * 10)}
+            />
+          </li>
+          <li className="home-calculator__items">
+            <CustomButton
+              id="home-calculator__button"
+              name={`Quero Economizar R$ ${total ? total : '200,00'}`}
+            />
+          </li>
+        </ul>
+      </form>
+    </div>
+  );
+};
+
+export default DiscontCalculator;
