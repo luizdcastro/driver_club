a1e533392adaed4c5a1959c374b8498f07380ff4
unknown <luizf.dcastro@gmail.com>
2020-08-09 16:00:07 -0300

    goome style

--- src/pages/home/home.page.js
+++ src/pages/home/home.page.js
@@ -9,11 +9,17 @@ const Home = () => {
   return (
     <div className="home-page__container">
       <div className="home-page__hero">
-        <div>
-          <h1> O melhor clube de beneficios para motoristas Uber e 99.</h1>
-          <h3> Reduza seus gastos mensais utilizando descontos exclusivos</h3>
-          <CustomButton id="driver-button" name="Sou Motorista" />
-          <CustomButton id="partner-button" name="Tenho um Negócio" />
+        <div className="hero-main">
+          <h1 className="hero-main__title">
+            O melhor clube de beneficios para motoristas Uber e 99.
+          </h1>
+          <h3 className="hero-main__subtitle">
+            Reduza seus gastos mensais utilizando descontos exclusivos
+          </h3>
+          <div className="hero-button">
+            <CustomButton id="custom-button" name="Sou Motorista" />
+            <CustomButton id="custom-button" name="Tenho um Negócio" />
+          </div>
         </div>
         <div>
           <DiscontCalculator />
