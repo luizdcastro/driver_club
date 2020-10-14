48c0a98848da446ccd76ff9c02f08473b8810569
unknown <luizf.dcastro@gmail.com>
2020-10-13 22:51:04 -0300

    api host change

--- src/services/api-middleware.js
+++ src/services/api-middleware.js
@@ -1,47 +1,46 @@
-import axios from 'axios';
+import axios from "axios";
 
-import * as constants from '../redux/constants';
-import { logoutUser } from '../redux/actions/auth.actions';
+import * as constants from "../redux/constants";
+import { logoutUser } from "../redux/actions/auth.actions";
 
 export const apiMiddleware = ({ dispatch, getState }) => (next) => (action) => {
-  if (action.type !== constants.API) return next(action);
+	if (action.type !== constants.API) return next(action);
 
-  dispatch({ type: constants.TOGGLE_LOADER });
-  const BASE_URL = 'https://rocketcab-server.herokuapp.com/api/v1';
-  const AUTH_TOKEN = getState().user.token;
-  if (AUTH_TOKEN)
-    axios.defaults.headers.common['Authorization'] = `Bearer ${AUTH_TOKEN}`;
+	dispatch({ type: constants.TOGGLE_LOADER });
+	const BASE_URL = "https://api.rocketcab.com.br/api/v1";
+	const AUTH_TOKEN = getState().user.token;
+	if (AUTH_TOKEN)
+		axios.defaults.headers.common["Authorization"] = `Bearer ${AUTH_TOKEN}`;
 
-  const {
-    url,
-    method,
-    success,
-    data,
-    onUploadProgress,
-    postProccessSuccess,
-    postProccessError,
-  } = action.payload;
+	const {
+		url,
+		method,
+		success,
+		data,
+		onUploadProgress,
+		postProccessSuccess,
+		postProccessError,
+	} = action.payload;
 
-  axios({
-    method,
-    url: BASE_URL + url,
-    data: data ? data : null,
-    onUploadProgress: onUploadProgress,
-  })
-    .then((response) => {
-      dispatch({ type: constants.TOGGLE_LOADER });
-      if (success) dispatch(success(response.data));
-      if (postProccessSuccess) postProccessSuccess(response.data);
-    })
-    .catch((err) => {
-      dispatch({ type: constants.TOGGLE_LOADER });
-      if (!err.response) console.log(err);
-      else {
-        if (err.response.data.status) {
-          if (err.response && err.response.status === 403)
-            dispatch(logoutUser());
-          if (postProccessError) postProccessError(err.response.data.status);
-        }
-      }
-    });
+	axios({
+		method,
+		url: BASE_URL + url,
+		data: data ? data : null,
+		onUploadProgress: onUploadProgress,
+	})
+		.then((response) => {
+			dispatch({ type: constants.TOGGLE_LOADER });
+			if (success) dispatch(success(response.data));
+			if (postProccessSuccess) postProccessSuccess(response.data);
+		})
+		.catch((err) => {
+			dispatch({ type: constants.TOGGLE_LOADER });
+			if (!err.response) console.log(err);
+			else {
+				if (err.response.data.status) {
+					if (err.response && err.response.status === 403) dispatch(logoutUser());
+					if (postProccessError) postProccessError(err.response.data.status);
+				}
+			}
+		});
 };
